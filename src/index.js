28a91bf4429bda5e5b9e056afac0de3578dc9977
Kurniawan <kurnhyalcantara@gmail.com>
2022-11-08 18:58:54 +0800

    [Testing] Add service worker functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { DataProvider } from "./GlobalState";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { DataProvider } from './GlobalState';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import "sweetalert2/src/sweetalert2.scss";
 
 ReactDOM.render(
@@ -15,8 +16,13 @@ ReactDOM.render(
       <App />
     </DataProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
