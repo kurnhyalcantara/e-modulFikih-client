2fa84b92fea8507bf39cc0608ee6e3bcafedc51c
Kurniawan <kurnhyalcantara@gmail.com>
2022-11-20 11:42:01 +0800

    FOoter finish

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,17 +1,19 @@
-import { Container, Grid, Link, Typography } from "@mui/material";
-import React from "react";
-import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import AddLocationIcon from "@mui/icons-material/AddLocation";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import PinterestIcon from "@mui/icons-material/Pinterest";
-import { useStyle } from "./styles";
+import { Container, Grid, Link, Typography } from '@mui/material';
+import React from 'react';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import { useStyle } from './styles';
+import { useTheme } from '@mui/material/styles';
 
 const Footer = () => {
   const classes = useStyle();
+  const theme = useTheme();
   return (
     <div className={classes.root}>
       <div className={classes.footers}>
@@ -21,26 +23,21 @@ const Footer = () => {
               <Grid container spacing={2}>
                 <Grid item xs={12}>
                   <Typography
-                    className={classes.menubutton}
-                    color="red"
-                    style={{ textDecoration: 'none' }}
-                    fontWeight="700"
-                    fontSize="25px"
-                    fontFamily="URW Chancery L, cursive"
-                    to="/"
-                    component={Link}
+                    color={theme.palette.primary.main}
+                    fontWeight="900"
+                    fontSize="32px"
                   >
-                    E-Learn
+                    Fikih MTs Bontouse
                   </Typography>
                 </Grid>
-                <Grid item xs={12}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Gravida viverra sodales nibh quisque tincidunt. Dolor
-                  elementum elementum non vitae turpis mauris adipiscing.
+                <Grid className={classes.description} item xs={12}>
+                  Modul Pembelajaran Fikih Berbasis
+                  <em> Progressive Web Apps </em>
+                  untuk siswa MTs As'adiyah No. 2 Bontouse
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item xs={6} md={2}>
+            <Grid item xs={6} md={3}>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
                   <p className={classes.heading}>Useful Links</p>
@@ -53,45 +50,11 @@ const Footer = () => {
                   </Link>
                   <br />
                   <Link underline="none" component="button">
-                    Love Mondays
+                    IAI As'adiyah Sengkang
                   </Link>
                   <br />
                   <Link underline="none" component="button">
                     For developers
-                  </Link>
-                  <Link underline="none" component="button">
-                    Press office
-                  </Link>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={6} md={2}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <p className={classes.heading}>Courses</p>
-                  <Link underline="none" component="button">
-                    Find a course
-                  </Link>
-                  <br />
-                  <Link underline="none" component="button">
-                    Career guides
-                  </Link>
-                  <br />
-                  <Link underline="none" component="button">
-                    Discount courses
-                  </Link>
-                  <br />
-                  <Link underline="none" component="button">
-                    Online courses
-                  </Link>
-                  <Link underline="none" component="button">
-                    Free courses
-                  </Link>
-                  <Link underline="none" component="button">
-                    Discount codes
-                  </Link>
-                  <Link underline="none" component="button">
-                    On Demand Terms
                   </Link>
                 </Grid>
               </Grid>
@@ -99,14 +62,14 @@ const Footer = () => {
             <Grid item xs={12} md={3}>
               Contact US
               <p className={classes.contact}>
-                <LocalPhoneIcon className={classes.ico} /> +00 123 456 789
+                <LocalPhoneIcon className={classes.ico} /> +62 81920 9382
               </p>
               <p className={classes.contact}>
-                <MailOutlineIcon className={classes.ico} /> info@elearn.inc
+                <MailOutlineIcon className={classes.ico} />{' '}
+                kurnhyalcantara@gmail.com
               </p>
               <p className={classes.contact}>
-                <AddLocationIcon className={classes.ico} /> lorem ipsum Road,
-                London, United Kingdom
+                <AddLocationIcon className={classes.ico} /> Sengkang, Wajo
               </p>
               <div className={classes.icons}>
                 <FacebookIcon className={classes.icon} />
@@ -122,24 +85,8 @@ const Footer = () => {
       <div className={classes.footer}>
         <Container maxWidth="xl">
           <Grid container spacing={2}>
-            <Grid item xs={12} md={7}>
-              <Grid container spacing={2}>
-                <Grid item xs={6} md={3}>
-                  Secuirity
-                </Grid>
-                <Grid item xs={6} md={3}>
-                  Privacy
-                </Grid>
-                <Grid item xs={6} md={3}>
-                  Cookie setting
-                </Grid>
-                <Grid item xs={6} md={3}>
-                  Terms & condition
-                </Grid>
-              </Grid>
-            </Grid>
             <Grid item xs={12} md={5}>
-              COPYRIGHT © elearn.com 2021
+              Copyright © Kurniawan 2022
             </Grid>
           </Grid>
         </Container>
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,49 +1,52 @@
-import { makeStyles } from "@mui/styles";
+import { makeStyles } from '@mui/styles';
 
 export const useStyle = makeStyles({
-    root: {
-        marginTop: "50px",
-        flexGrow: 1,
-    },
-    footers: {
-        paddingBottom: "10px",
-        backgroundColor: "#FFF1F1",
-    },
-    branding: {
-        display: "flex",
-        alignItems: "center",
-    },
-    heading: {
-        margin: "0px",
-        padding: "0px",
-        fontWeight: 400,
-        fontSize: "16px",
-        fontFamily: "mulish",
-        paddingBottom: "10px",
-
-    },
-    footer: {
-        marginTop: "20px",
-        paddingBottom: "15px",
-        backgroundColor: "#EA5252",
-        textAlign: "center",
-        color: "white",
-    },
-    ico: {
-        color: "#EA5252",
-    },
-    contact: {
-        display: "flex",
-        alignItems: "center",
-    },
-    icons: {
-        color: "#EA5252",
-    },
-    icon: {
-        margin: "6px",
-        border: "0.5px solid #EA5252",
-    },
-    menubutton: {
-
-    }
+  root: {
+    marginTop: '50px',
+    flexGrow: 1,
+  },
+  footers: {
+    paddingBottom: '10px',
+    backgroundColor: '#FDECDF',
+  },
+  branding: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  description: {
+    fontSize: '14px',
+    color: '#2C3644',
+  },
+  heading: {
+    margin: '0px',
+    padding: '0px',
+    fontWeight: 400,
+    fontSize: '16px',
+    paddingBottom: '10px',
+  },
+  footer: {
+    marginTop: '20px',
+    paddingBottom: '15px',
+    backgroundColor: '#006f59',
+    textAlign: 'center',
+    color: 'white',
+  },
+  ico: {
+    color: '#ff9800 !important',
+    marginRight: '10px',
+  },
+  contact: {
+    color: '#2C3644',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  icons: {
+    color: '#ff9800',
+  },
+  icon: {
+    margin: '6px',
+    border: '0.5px solid #ff9800',
+  },
+  menubutton: {},
 });
